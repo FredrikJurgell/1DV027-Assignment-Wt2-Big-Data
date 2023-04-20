9c9598362a66ffbef3fbca010a5291e86d1c92d8
Fredrik Jurgell <fj222qp@student.lnu.se>
2023-04-20 09:28:18 +0200

    Added functionality to fetch data from elasticsearch

---
+++ src/controllers/HomeController.js
@@ -0,0 +1,88 @@
+/**
+ * Module for the HomeController.
+ *
+ * @author Fredrik Jurgell
+ * @version 1.0.0
+ */
+
+import https from 'https'
+import fetch from 'node-fetch'
+
+/**
+ * Controller for the home page.
+ */
+export class HomeController {
+  /**
+   * Renders the home page with genre statistics.
+   *
+   * @param {object} req - The HTTP request.
+   * @param {object} res - The HTTP response.
+   * @param {Function} next - The next middleware function.
+   */
+  async index (req, res, next) {
+    try {
+      const username = 'elastic'
+      const password = process.env.ELASTIC_PASSWORD
+      const query = {
+        size: 1000,
+        aggs: {
+          genres: {
+            terms: {
+              field: 'genre.keyword'
+            }
+          }
+        }
+      }
+
+      const url = 'https://192.168.1.121:9200/imdbdata/_search'
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(query),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
+        },
+        agent: new https.Agent({ rejectUnauthorized: false })
+      })
+
+      const data = await response.json()
+      const movies = data.hits.hits
+      const genreCount = countGenres(movies)
+      console.log(genreCount)
+
+      // Socket.io: Send the created issue to all subscribers.
+      res.io.emit('genreCount', {
+        genreCount
+      })
+
+      res.render('home/index', genreCount)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+/**
+ * Counts the number of movies per genre.
+ *
+ * @param {Array} movies - The array of movies.
+ * @returns {object} An object containing the count of movies per genre.
+ */
+function countGenres (movies) {
+  const genreCount = {}
+  // Loop through every movie
+  for (let i = 0; i < movies.length; i++) {
+    // Split every genre-string into a array of gengre
+    let genres = movies[i]._source.genre.split(', ')
+    // Loop through every genre
+    for (let j = 0; j < genres.length; j++) {
+      if (genreCount[genres[j]]) {
+        genreCount[genres[j]]++
+      } else {
+        genreCount[genres[j]] = 1
+      }
+    }
+  }
+
+  return genreCount
+}
