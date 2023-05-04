034beed6ba43a6a72663ebcfdba69a648957fe49
Fredrik Jurgell <fj222qp@student.lnu.se>
2023-05-04 13:16:21 +0200

    Added comments to AddToElastic.cs

--- AddToElastic.cs
+++ AddToElastic.cs
@@ -1,28 +1,34 @@
-using System;
+/*
+This code defines a class AddToElastic that allows to add data to an ElasticSearch index.
+*/
 using Nest;
 
 namespace assignment_wt2_oauth
 {
-    public class AddToElastic
+  public class AddToElastic
     {
     private readonly IElasticClient elasticClient;
-
+    /*
+       AddToElastic constructor takes an IElasticClient instance and sets it to a private variable.
+    */
     public AddToElastic(IElasticClient elasticClient)
     {
         this.elasticClient = elasticClient;
     }
 
+    /*
+       AddData method allows to add an IEnumerable of Data to an ElasticSearch index and sends the data to ElasticSearch in batches.
+    */
     public async Task AddData(IEnumerable<Data> datas){
-
             var index = "imdbdata";
             var batchSize = 200;
-            var shiped = 0;
+            var shipped = 0;
 
-            while (datas.Skip(shiped).Take(batchSize).Any())
+            while (datas.Skip(shipped).Take(batchSize).Any())
             {
-                var batch = datas.Skip(shiped).Take(batchSize);
+                var batch = datas.Skip(shipped).Take(batchSize);
                 var response = await elasticClient.BulkAsync(b => b.CreateMany(batch).Index(index));
-                shiped += batchSize;
+                shipped += batchSize;
             }
         }
     }
